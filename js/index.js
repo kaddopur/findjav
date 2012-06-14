8899ce3df56ba27bc9f0841e993338a316092e64
Jason C. Huang <chaoju.huang@gmail.com>
2012-06-14 23:12:32 +0800

    - use vid from DMM R18 directly

--- js/index.js
+++ js/index.js
@@ -1,45 +1,71 @@
 (function() {
+  var search_vid, show_result, unwrap;
+
+  unwrap = function(input) {
+    var output, r;
+    r = /([a-zA-Z]+)0*(\d+)/;
+    output = r.exec(input);
+    if (output) {
+      while (output[2].length < 3) {
+        output[2] = '0' + output[2];
+      }
+      return output[1] + output[2];
+    } else {
+      return '';
+    }
+  };
+
+  show_result = function(itype, body, atype) {
+    if (window.success !== 'success') {
+      $('i').removeClass().addClass(itype);
+      $('.alert-body').html(body);
+      $('.alert').removeClass().addClass("alert alert-" + atype + " fade in").fadeIn();
+      return $('.preview').hide();
+    }
+  };
+
+  search_vid = function(target) {
+    return $.get('http://javjunkies.com/main/?s=' + target, function(data) {
+      var image_link, q, r, re;
+      console.log(target);
+      re = /Not Found/;
+      if (!re.exec(data)) {
+        re = /\&#038;(file=.*torrent)/;
+        q = re.exec(data)[1];
+        re = new RegExp("Video Id: " + target + "<");
+        r = re.exec(data);
+        if (q && r) {
+          re = new RegExp("" + target + "\.torrent.*(http.*jpg)");
+          image_link = re.exec(data)[1];
+          $('.alert').fadeOut('fast', function() {
+            show_result('icon-ok', "" + target + " is <a href='http://javjunkies.com/main/JavJ.php?k=1202&" + q + "'>HERE</a>.", 'success');
+            return $('.preview').attr('src', image_link).show();
+          });
+        }
+      } else {
+        return $('.alert').fadeOut('fast', function() {
+          return show_result('icon-remove', "" + target + " doesn't exist.", 'error');
+        });
+      }
+    }).error(function() {
+      return $('.alert').fadeOut('fast', function() {
+        return show_result('icon-ban-circle', 'is temporary unavailable. Please try again later.', 'block');
+      });
+    });
+  };
 
   $(function() {
     $('.alert').hide();
     return $('input').focus().keydown(function(event) {
-      var target;
+      var vinfo;
       if (event.which === 13 && $('input').val()) {
-        target = $('input').val().toUpperCase();
-        return $.get('http://javjunkies.com/main/?s=' + target, function(data) {
-          var image_link, q, r, re;
-          re = /Not Found/;
-          if (!re.exec(data)) {
-            re = /\&#038;(file=.*torrent)/;
-            q = re.exec(data)[1];
-            re = new RegExp("Video Id: " + target + "<");
-            r = re.exec(data);
-            if (q && r) {
-              re = new RegExp("" + target + "\.torrent.*(http.*jpg)");
-              image_link = re.exec(data)[1];
-              $('.alert').fadeOut('fast', function() {
-                $('i').removeClass().addClass('icon-ok');
-                $('.alert-body').html("" + target + " is <a href='http://javjunkies.com/main/JavJ.php?k=1202&" + q + "'>HERE</a>.");
-                $('.alert').removeClass().addClass('alert alert-success fade in').fadeIn();
-                return $('.preview').attr('src', image_link).show();
-              });
-              return;
-            }
-          }
-          return $('.alert').fadeOut('fast', function() {
-            $('i').removeClass().addClass('icon-remove');
-            $('.alert-body').html("" + target + " doesn't exist.");
-            $('.alert').removeClass().addClass('alert alert-error fade in').fadeIn();
-            return $('.preview').hide();
+        vinfo = unwrap($('input').val().toUpperCase());
+        if (!vinfo) {
+          $('.alert').fadeOut('fast', function() {
+            show_result('icon-remove', "" + ($('input').val()) + " doesn't exist.", 'error');
           });
-        }).error(function() {
-          return $('.alert').fadeOut('fast', function() {
-            $('i').removeClass().addClass('icon-ban-circle');
-            $('.alert-body').html("is temporary unavailable. Please try again later.");
-            $('.alert').removeClass().addClass('alert alert-block fade in').fadeIn();
-            return $('.preview').hide();
-          });
-        });
+        }
+        return search_vid(vinfo);
       }
     });
   });
--- src/index.coffee
+++ src/index.coffee
@@ -1,35 +1,52 @@
+unwrap = (input) ->
+  r = /([a-zA-Z]+)0*(\d+)/
+  output = r.exec(input)
+  if output
+    while output[2].length < 3
+      output[2] = '0' + output[2]
+    output[1] + output[2]
+  else
+    ''
+
+show_result = (itype, body, atype) ->
+  if window.success != 'success'
+    $('i').removeClass().addClass(itype)
+    $('.alert-body').html(body)
+    $('.alert').removeClass().addClass("alert alert-#{atype} fade in").fadeIn()
+    $('.preview').hide()
+
+search_vid = (target) ->
+  $.get('http://javjunkies.com/main/?s='+target, (data) ->
+    console.log target
+    re = /Not Found/
+    if not re.exec(data)
+      re = /\&#038;(file=.*torrent)/
+      q = re.exec(data)[1]
+      re = new RegExp("Video Id: #{target}<")
+      r = re.exec(data)
+      if q and r
+        #re = new RegExp("src=\"(.*#{target.toLowerCase()}.*jpg)\"")
+        re = new RegExp("#{target}\.torrent.*(http.*jpg)")
+        image_link = re.exec(data)[1]
+        $('.alert').fadeOut 'fast', ->
+          show_result('icon-ok', "#{target} is <a href='http://javjunkies.com/main/JavJ.php?k=1202&#{q}'>HERE</a>.", 'success')
+          $('.preview').attr('src', image_link).show()
+        return
+    else
+      $('.alert').fadeOut 'fast', ->
+        show_result('icon-remove', "#{target} doesn't exist.", 'error')
+  ).error( ->
+    $('.alert').fadeOut 'fast', ->
+      show_result('icon-ban-circle', 'is temporary unavailable. Please try again later.', 'block')
+  )
+
 $ ->
   $('.alert').hide()
-  $('input').focus().keydown (event)->
+  $('input').focus().keydown (event) ->
     if event.which == 13 and $('input').val()
-      target = $('input').val().toUpperCase()
-      $.get('http://javjunkies.com/main/?s='+target, (data)->
-        re = /Not Found/
-        if not re.exec(data)
-          re = /\&#038;(file=.*torrent)/
-          q = re.exec(data)[1]
-          re = new RegExp("Video Id: #{target}<")
-          r = re.exec(data)
-          if q and r
-            #re = new RegExp("src=\"(.*#{target.toLowerCase()}.*jpg)\"")
-            re = new RegExp("#{target}\.torrent.*(http.*jpg)")
-            image_link = re.exec(data)[1]
-            $('.alert').fadeOut 'fast', ->
-              $('i').removeClass().addClass('icon-ok')
-              $('.alert-body').html("#{target} is <a href='http://javjunkies.com/main/JavJ.php?k=1202&#{q}'>HERE</a>.")
-              $('.alert').removeClass().addClass('alert alert-success fade in').fadeIn()
-              $('.preview').attr('src', image_link).show()
-            return
+      vinfo = unwrap($('input').val().toUpperCase())
+      if not vinfo
         $('.alert').fadeOut 'fast', ->
-          $('i').removeClass().addClass('icon-remove')
-          $('.alert-body').html("#{target} doesn't exist.")
-          $('.alert').removeClass().addClass('alert alert-error fade in').fadeIn()
-          $('.preview').hide()
-      ).error( ->
-        $('.alert').fadeOut 'fast', ->
-          $('i').removeClass().addClass('icon-ban-circle')
-          $('.alert-body').html("is temporary unavailable. Please try again later.")
-          $('.alert').removeClass().addClass('alert alert-block fade in').fadeIn()
-          $('.preview').hide()
-      )
-
+          show_result('icon-remove', "#{$('input').val()} doesn't exist.", 'error')
+          return
+      search_vid(vinfo)
